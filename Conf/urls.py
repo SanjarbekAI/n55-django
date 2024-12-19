164c0b440a5cc72142f5b2e366786e1c64976034
SanjarbekAI <sanjarbekwork@gmail.com>
2024-12-19 15:14:41 +0500

    static files settings

--- Conf/urls.py
+++ Conf/urls.py
@@ -1,9 +1,15 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
-
-from blogs.views import home_page_view
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/', home_page_view)
+    path('blogs/', include('blogs.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+    document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT)
